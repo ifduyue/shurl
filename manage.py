1573061444e1a12f28a0a06eecd8f9defca72727
lyxint <lyxint@gmail.com>
2012-04-15 15:36:52 +0800

    manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,11 @@ def view(uid):
     path = get_url_path(uid)
     print uid, readfrom(path)
 
+def viewall():
+    count = get_url_count()
+    for i in xrange(1, count+1):
+        view(base62_encode(i)
+
 def delete(uid):
     path = get_url_path(uid)
     url = readfrom(url)
@@ -18,7 +23,7 @@ def delete(uid):
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option("-a", "--action", dest="action", default="view",
-                      help="Which action to perform: [view, delete]")
+                      help="Which action to perform: [view, viewall, delete]")
 
     options, args = parser.parse_args()
     
