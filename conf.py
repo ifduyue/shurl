e857550040576662dd91424ded01c47c65417f4d
lyxint <lyxint@gmail.com>
2012-04-15 15:04:10 +0800

    import data from plain file to postgresql

--- conf.py
+++ conf.py
@@ -2,6 +2,11 @@ import os
 
 CACHE_SIZE = 1000
 ROOT_DIR = os.path.realpath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+URL_DIR = os.path.join(DATA_DIR, 'url')
+HASH_DIR = os.path.join(DATA_DIR, 'hash') 
+LOCK_FILE = os.path.join(DATA_DIR, 'lock')
+UID_FILE = os.path.join(DATA_DIR, 'uid')
 DOMAIN = 'shurl.im'
 
 psql_db = 'dbname=shurl user=postgres password=nicai'
---
+++ import_data.py
@@ -0,0 +1,40 @@
+import conf
+from lib import pool
+from base62 import base62_encode
+
+def readfrom(path):
+    try:
+        with open(path, 'r') as f:
+            return f.read()
+    except: pass
+    return None
+
+def get_uid_count():
+    try:
+        uid = readfrom(conf.UID_FILE)
+        return int(uid) - 1
+    except: return 0
+
+def split_path(uid):
+    length = len(uid)
+    if len(uid) <= 2:
+        return os.path.join('_', uid)
+    if len(uid) <= 6:
+        return os.path.join('__', uid[0], uid[1:3], uid[3:] or uid)
+    return os.path.join(uid[0], uid[1:3], uid[3:5], uid[5:])
+
+def get_hash_path(hash):
+    return os.path.join(conf.HASH_DIR, split_path(hash))
+
+def get_url_path(uid):
+    return os.path.join(conf.URL_DIR, split_path(uid))
+
+if __name__ == '__main__':
+    count = get_uid_count()
+    for i in xrange(1, count+1):
+        uid = base62_encode(i)
+        path = get_url_path(uid)
+        url = readfrom(path)
+        ret = pool.execute("insert into shurl (id, url) values (%s, %s)", (i, url))
+        print ret, i, url
+
